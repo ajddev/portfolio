adc4d807fd25b936af26502d920d2deb629f9b88
Anthony Dube <dubedeveloper@gmail.com>
2022-05-14 13:18:36 -0400

    added contact form and animated svg

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // const svg = document.getElementById("my-svg");
+const loadContainer = document.querySelector(".load-container");
 const overlay = document.getElementById("overlay");
 
 function toggleMenu() {
   overlay.classList.toggle("visible");
+}
+
+overlay.addEventListener("click", function (e) {
+  toggleMenu();
+});
+
+function loaded() {
+  loadContainer.classList.add("hidden");
+  // fade out and hide loader
+  // enable overflow-y
 }
